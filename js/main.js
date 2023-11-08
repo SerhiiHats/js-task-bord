2be65343089b9b1be124fd586760edf65fcc2fc8
Serhi Hats <gatsserv@gmail.com>
2023-11-08 23:08:52 +0200

    change style and add listener for blur and edit

--- js/main.js
+++ js/main.js
@@ -14,15 +14,24 @@ function addTask() {
   btn.addEventListener('click', () => {
     form.style.display = 'block';
     btn.style.display = 'none';
-    addBtn.style.display = 'none';
+    // addBtn.style.display = 'none';
+    textarea.focus();
 
     textarea.addEventListener('input', (e) => {
       value = e.target.value;
 
-      if (value) {
-        addBtn.style.display = 'flex';
-      } else {
-        addBtn.style.display = 'none';
+      // if (value) {
+      //   addBtn.style.display = 'flex';
+      // } else {
+      //   addBtn.style.display = 'none';
+      // }
+    })
+
+    textarea.addEventListener('blur', ()=>{
+      if (!value){
+        form.style.display = 'none';
+        btn.style.display = 'flex';
+        // addBtn.style.display = 'block';
       }
     })
   });
@@ -36,32 +45,41 @@ function addTask() {
   });
 
   addBtn.addEventListener('click', () => {
-    const newItem = document.createElement('div');
-    newItem.classList.add('list__item');
-    newItem.draggable = true;
-    newItem.textContent = value;
+    if(value){
+      const newItem = document.createElement('div');
+      newItem.classList.add('list__item');
+      newItem.draggable = true;
+      newItem.textContent = value;
 
-    lists[0].append(newItem);
-    textarea.value = '';
-    value = '';
-    form.style.display = 'none';
-    btn.style.display = 'flex';
+      lists[0].append(newItem);
+      textarea.value = '';
+      value = '';
+      form.style.display = 'none';
+      btn.style.display = 'flex';
 
-    dragNDrop();
+      dragNDrop();
+    }
+
   });
 }
 
 addTask();
 
 function addBoard() {
-  const boards = document.querySelector('.boards');
+  // const boards = document.querySelector('.boards');
   const newBoard = document.createElement('div');
   newBoard.classList.add('boards__item');
   newBoard.innerHTML = `
-   <span contenteditable="true" class="title">Enter name of your board</span>
+   <span class="title">
+       <span contenteditable="true" class="title__text">Enter name of your board</span>
+       <i class="fa-regular fa-pen-to-square"></i>
+   </span>
    <div class="list"></div>
 `
-  boards.append(newBoard);
+  // todo: add correct title
+
+  btnAddBoards.before(newBoard);
+  // boards.append(newBoard);
   changeTitle();
   dragNDrop();
 }
@@ -69,9 +87,26 @@ function addBoard() {
 btnAddBoards.addEventListener('click', addBoard);
 
 function changeTitle() {
-  const titles = document.querySelectorAll('.title');
-  titles.forEach(title => {
-    title.addEventListener('click', e => e.target.textContent = '')
+  const titles = document.querySelectorAll('.title__text');
+
+  titles.forEach(titleText => {
+
+    //------- click title  -----------
+    titleText.addEventListener('click', e => e.target.textContent = '')
+
+    //------- lost focus title  -----------
+    titleText.addEventListener('blur', e => {
+      if (!e.target.textContent) {
+        e.target.textContent = 'Enter name of your board';
+      }
+    });
+
+   //------- click icon edit  -----------
+    titleText.nextElementSibling.addEventListener('click', (e) => {
+      e.target.previousElementSibling.click();
+      e.target.previousElementSibling.focus();
+    });
+
   });
 }
 
@@ -107,29 +142,26 @@ function dragNDrop() {
   }
 
   for (const list of lists) {
-  //
-  // }
-  // for (let j = 0; j < lists.length; j++) {
-  //   const list = lists[j];
+
     list.addEventListener('dragover', (e) => e.preventDefault());
 
-    list.addEventListener('dragenter', function(e){
+    list.addEventListener('dragenter', function (e) {
       this.style.backgroundColor = "rgba(155, 155, 155, 0.3)";
 
     });
 
-    list.addEventListener('dragleave', function(e){
+    list.addEventListener('dragleave', function (e) {
       this.style.backgroundColor = "rgba(0, 0, 0, 0)";
     });
 
-    list.addEventListener('drop', function(e){
+    list.addEventListener('drop', function (e) {
       this.style.backgroundColor = "rgba(0, 0, 0, 0)";
       this.append(draggedItem);
-      // draggedItem = null;
     });
 
   }
 
 }
+
 
 dragNDrop();
